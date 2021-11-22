514c66544d9343b0e625628e84f58f52bd387fb8
Minh <minhnha@smartosc.com>
2021-11-22 21:22:20 +0700

    update profile

--- src/contants/api.js
+++ src/contants/api.js
@@ -9,7 +9,7 @@ const API = {
     CAMPAIGNS: `${COMPANY_SERVICE}/campaigns`,
     CAMPAIGN: (id) => `${COMPANY_SERVICE}/campaign/${id}`,
     BANNERS: `${COMPANY_SERVICE}/banners`,
-    PROFILE: `${COMPANY_SERVICE}/artist/1`
+    PROFILE: `${COMPANY_SERVICE}/artist-info/1`
   },
 };
 
